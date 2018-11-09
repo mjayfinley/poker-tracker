636db7021be6dec9ea28a3dda87dbfcdd02d8206
Micah Finley <mjayfinley@gmail.com>
2018-11-08 23:12:07 -0600

    boiler plate and readme

---
+++ src/Components/Bankroll.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export class Bankroll extends Component {
+  render() {
+    return (
+      <div>
+       Bankroll
+      </div>
+    );
+  }
+}
+
+
---
+++ src/Components/Statistics.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export class Statistics extends Component {
+  render() {
+    return (
+      <div>
+       Statistics
+      </div>
+    );
+  }
+}
+
+
